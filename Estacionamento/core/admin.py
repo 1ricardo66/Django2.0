d4326cae9e314571ab0f4ab21b75f183947bf8b4
Ricardo <ricardofps66@gmail.com>
2018-08-27 10:58:54 -0300

    Adicionado classe MovRotativo

--- Estacionamento/core/admin.py
+++ Estacionamento/core/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import (Cliente, Carro , Alugar,
-                                            )
+from .models import (Cliente,
+                     Veiculo,
+                     Parametros,
+                     MovRotativo)
+
+class MovRotativoAdmin(admin.ModelAdmin):
+    list_display = ("checkin","checkout","valor_hora","pago","total","horas_total")
 
 admin.site.register(Cliente)
-admin.site.register(Carro)
-admin.site.register(Alugar)
+admin.site.register(Veiculo)
+admin.site.register(Parametros)
+admin.site.register(MovRotativo,MovRotativoAdmin)
 # Register your models here.
--- Estacionamento/core/models.py
+++ Estacionamento/core/models.py
@@ -1,6 +1,13 @@
 from django.db import models
-
-
+import math
+#############################################################
+#                                                           #
+#                                                           #
+#       IMPLEMENTAR UM METODO DE CATEGORIA DE CARROS        #
+#       DEPENDENDO DA CATEGORIA MUDA O PREÇO DA HORA        #
+#                                                           #
+#                                                           #
+#############################################################
 class Cliente(models.Model):
     nome = models.CharField(max_length=60)
     data_de_nascimento = models.DateField()
@@ -13,7 +20,7 @@ class Cliente(models.Model):
         return self.nome
 
 
-class Carro(models.Model):
+class Veiculo(models.Model):
         nome = models.CharField(max_length=120,null=True,blank=True)
         modelo = models.CharField(max_length=120)
         placa = models.CharField(max_length=9)
@@ -23,23 +30,31 @@ class Carro(models.Model):
             content = (self.nome + ' - ' + self.placa)
             return content
 
-class Alugar(models.Model):
-    STATUS_CHOICES = (
-    ('Alugado','Alugado'),
-    ('Reservado','Reservado'),
-    ('Encerrado','Encerrado'),
-    ('Completo','Completo'),
-    )
-    cliente = models.ForeignKey(Cliente,on_delete=models.CASCADE)
-    carro = models.ForeignKey(Carro,on_delete=models.CASCADE)
-    alugado = models.DateField()
-    devolucao = models.DateField()
-    status = models.CharField(default='Completo',choices=STATUS_CHOICES,max_length=10)
-
-
-    class Meta:
-        verbose_name_plural = 'Alugar'
+class Parametros(models.Model):
+    valor_hora = models.DecimalField(max_digits=5,decimal_places=2)
+    valor_mes = models.DecimalField(max_digits=5,decimal_places=2)
 
     def __str__(self):
-        return self.cliente
+        return "Parametros Gerais"
+
+
+class MovRotativo(models.Model):
+    cliente = models.ForeignKey(Cliente,on_delete=models.CASCADE)
+    checkin = models.DateTimeField(auto_now=False)
+    checkout = models.DateTimeField(auto_now=False)
+    valor_hora = models.DecimalField(max_digits=5,decimal_places=2)
+    veiculo = models.ForeignKey(Veiculo,on_delete=models.CASCADE)
+    pago = models.BooleanField(default=False)
+
+    def horas_total(self):
+
+        return math.ceil((self.checkout - self.checkin).total_seconds() / 3600)
+
+
+    def total (self):
+        self.valor_total = self.valor_hora * self.horas_total()
+        return ("%s R$"%self.valor_total)
+
+    def __str__(self):
+        return self.veiculo.placa
 # Create your models here.
